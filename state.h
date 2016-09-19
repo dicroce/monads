867a84ef1210201bea373be96fad65502a0d661c
Tony Di Croce <dicroce@gmail.com>
2016-09-19 13:55:34 -0700

    Fixed typo.

--- state.h
+++ state.h
@@ -2,7 +2,7 @@
 #ifndef __monad_state_h
 #define __monad_state_h
 
-// terminal is a lambda that takes an argument, and returns another lambda that captures that argument. This innner
+// terminal_state is a lambda that takes an argument, and returns another lambda that captures that argument. This innner
 // lambda takes a function as an argument and passes the captured argument to it and returns its result.
 auto terminal_state = [] ( auto term ) {
     return [=] ( auto func ) {
@@ -11,7 +11,7 @@ auto terminal_state = [] ( auto term ) {
 };
 
 // fmap is a lambda that takes a lambda as an argument and returns a lambda that captures that argument. This inner
-// lambda takes an argument that it passes to the outer lambdas lambda... Then it calls terminal on the result and
+// lambda takes an argument that it passes to the outer lambdas lambda... Then it calls terminal_state on the result and
 // returns its result.
 auto fmap_state = [] ( auto f ) {
     return [=] ( auto t ) {
